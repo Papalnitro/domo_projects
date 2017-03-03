1d9d61da78aad8092c82b4b0ee1a49751cab5b49
Papalnitro <georges@rossopoulos.com>
2017-03-03 20:24:58 +0100

    Version that does read a dummy root directory

--- ds18b20_reader_class.php
+++ ds18b20_reader_class.php
@@ -3,8 +3,32 @@
 // 1-wire class to read temperatures
 
 class ds18b20_reader {
-
-
+    
+    // Dummy constructor
+    function __construct() {		
+    }
+    
+    // This member function returns the IDs found on the 1-wire
+    // Simply by reading the correct directory
+    public function get_list()
+    {
+        if ($handle = opendir('/')) {
+            while (false !== ($entry = readdir($handle))) {
+                $probe_list[]=$entry;
+            }
+            closedir($handle);
+        } else {
+            $probe_list=NULL;
+        }
+        
+        return $probe_list;
+    }
 }
+
+$probe= new ds18b20_reader();
+$res = $probe->get_list();
+
+var_dump($res);
+
 
 ?>
